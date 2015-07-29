e9d786cbe716e726780d0c9e60795b8b17b9767e
rprom <ritchie.prom@gmail.com>
2015-07-28 18:18:53 -0700

    Finished AboutMe/Languages/Application sections

--- ritchie_prom_com/Scripts/custom.js
+++ ritchie_prom_com/Scripts/custom.js
@@ -6,17 +6,32 @@
 
         // If scroll past a certain point, else...
         if (yPosition > pastPosition) {
-            $("#site-navbar").addClass("solid");
-            $(".menuItem a").removeClass("large");
-            $(".menuItem a").addClass("bold-font");
-            $("#navbar-download a").removeClass("display-hidden");
+            $('#site-navbar').addClass('solid');
+            $('.menu-item a').removeClass('large');
+            $('.menu-item a').addClass('bold-font');
+            $('#navbar-download a').removeClass('display-hidden');
         }
         else {
-            $("#site-navbar").removeClass("solid");
-            $(".menuItem a").addClass("large");
-            $(".menuItem a").removeClass("bold-font");
-            $("#navbar-download a").addClass("display-hidden");
+            $('#site-navbar').removeClass('solid');
+            $('.menu-item a').addClass('large');
+            $('.menu-item a').removeClass('bold-font');
+            $('#navbar-download a').addClass('display-hidden');
         }
+
+        // To activate 'active' link
+        var scrollPos = $(document).scrollTop();
+
+        $('#menu-container a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr('href'));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#menu-container ul li a').removeClass('active'); //added to remove active class from all a elements
+                currLink.addClass('active');
+            }
+            else {
+                currLink.removeClass('active');
+            }
+        });
     });
 
     // For smooth scrolling with anchor tags
@@ -38,17 +53,17 @@
     // For cycling through text in the objective section
     $(function () {
         var wordlist = [
-            "Web",
-            "Front-End",
-            "Back-End",
-            "C#",
-            "JavaScript"
+            'Web',
+            'Front-End',
+            'Back-End',
+            'C#',
+            'JavaScript'
         ]
 
         var count = 0;
 
         function changeWord() {
-            $("#objective .word-group h1").text(wordlist[count]);
+            $('#objective .word-group h1').text(wordlist[count]);
             count++;
 
             if (count > wordlist.length) {
